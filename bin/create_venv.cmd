8fa0433d000cc5af0e539c36f5e377635df994c5
ZD <zsvoboda@gmail.com>
2025-03-20 13:24:28 +0100

    Fix cmd

--- bin/create_venv.cmd
+++ bin/create_venv.cmd
@@ -3,9 +3,9 @@
 set THIS_DIR=%~dp0
 
 :: Create a virtual environment in the bin directory
-python -m venv %THIS_DIR%..\..venv
+python -m venv %THIS_DIR%..\.venv
 
 :: Activate the virtual environment
-call %THIS_DIR%..\..venv\Scripts\activate
+call %THIS_DIR%..\.venv\Scripts\activate
 
-echo Virtual environment created and activated in %THIS_DIR%..\..venv
+echo Virtual environment created and activated in %THIS_DIR%.\..venv
--- bin/install_python_packages.cmd
+++ bin/install_python_packages.cmd
@@ -3,9 +3,9 @@
 set THIS_DIR=%~dp0
 
 :: Activate the virtual environment
-call %THIS_DIR%..\..venv\Scripts\activate
+call %THIS_DIR%..\.venv\Scripts\activate
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-echo Virtual environment activated from %THIS_DIR%..\..venv and all required packages installed.
+echo Virtual environment activated from %THIS_DIR%..\.venv and all required packages installed.
