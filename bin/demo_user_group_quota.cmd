9fd67edddf3df6a95a8ad080a923d7ca799c3bcb
ZD <zsvoboda@gmail.com>
2025-04-04 14:08:04 +0200

    Multiprotocol

--- bin/demo_user_group_quota.cmd
+++ bin/demo_user_group_quota.cmd
@@ -6,6 +6,9 @@ set SRC_DIR=%THIS_DIR%\..\src
 set LIB_DIR=%THIS_DIR%\..\lib
 set PYTHONPATH=%LIB_DIR%;%SRC_DIR%
 
+:: Activate the virtual environment
+call %THIS_DIR%\..\.venv\Scripts\activate
+
 for /f "delims=" %%x in (%THIS_DIR%..\.env) do set %%x
 echo "Setting up Flash Array..."
 
--- bin/randcopy.cmd
+++ bin/randcopy.cmd
@@ -5,5 +5,7 @@ set SRC_DIR=%THIS_DIR%\..\src
 set LIB_DIR=%THIS_DIR%\..\lib
 set PYTHONPATH=%LIB_DIR%;%SRC_DIR%
 
+:: Activate the virtual environment
+call %THIS_DIR%\..\.venv\Scripts\activate
 
 python %SRC_DIR%\util\randcopy.py %*
