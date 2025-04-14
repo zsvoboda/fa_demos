c5947ab68e8e78e4ec117579b25145adf9dbe806
ZD <zsvoboda@gmail.com>
2025-04-14 13:53:55 +0200

    Fix NFS ACL setup for nfs_daemons in demo_multiprotocol script

--- bin/demo_multiprotocol.sh
+++ bin/demo_multiprotocol.sh
@@ -46,8 +46,8 @@ fi
 echo "Setting up NFS ACLs for shared_dir ..."
 echo nfs4_setfacl -a "A:gfd:win_users@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
 nfs4_setfacl -a "A:gfd:win_users@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
-echo nfs4_setfacl -a "A:gfd:win_users@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
-sudo nfs4_setfacl -a "A:gfd:win_users@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
+echo nfs4_setfacl -a "A:gfd:nfs_daemons@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
+sudo nfs4_setfacl -a "A:gfd:nfs_daemons@${DEMO_DOMAIN}:RW" /mnt/multi/shared_dir
 
 echo "Test content written from NFS mounted drive." > /mnt/multi/shared_dir/file_from_linux_nfs_session.txt
 
