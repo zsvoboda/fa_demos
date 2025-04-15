c092e862d8c6cad0d1b5c11cfdf246154750e4fa
ZD <zsvoboda@gmail.com>
2025-04-15 16:37:47 +0200

    Refactor setup_array.py to enhance logging and streamline policy management

--- src/demos/user_group_quota/setup_array.py
+++ src/demos/user_group_quota/setup_array.py
@@ -1,11 +1,11 @@
 import sys
 import os
 
-from pypureclient.flasharray import ReferenceWithType
+from ad.active_directory import ActiveDirectory
+from fa import FlashArray
+from pypureclient.flasharray.FA_2_X import ReferenceWithType
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-from fa.flash_array import FlashArray
 
 import logging
 from log import setup_logging
@@ -14,57 +14,19 @@ setup_logging()
 _logger = logging.getLogger(__name__)
 
 from load_env import load_env
-import requests
+
 load_env()
 
-def create_policy_user_group_quota(_policy_name):
-    
-    FA_HOSTNAME = os.getenv("FA_DEMO_HOSTNAME")
-    
-    url = f'http://{FA_HOSTNAME}/2.X/policies/user-group-quota?names={_policy_name}'
-    headers = {
-        'user': 'root',
-        'Content-Type': 'application/json'
-    }
-    data = {
-        "location_context": None,
-        "enabled": True
-    }
-
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code == 200:
-        _logger.info("User group quota policy created successfully.")
-    else:
-        _logger.error(f"Failed to create user group quota policy. Status code: {response.status_code}, Response: {response.text}")
-        raise Exception(f"Failed to create user group quota policy. Status code: {response.status_code}, Response: {response.text}")
-    
-def delete_policy_user_group_quota(_policy_name):
-    FA_HOSTNAME = os.getenv("FA_DEMO_HOSTNAME")
-
-    url = f'http://{FA_HOSTNAME}/2.X/policies/user-group-quota?names={_policy_name}'
-    headers = {
-        'user': 'root',
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.delete(url, headers=headers)
-
-    if response.status_code == 200:
-        _logger.info("User group quota policy deleted successfully.")
-    else:
-        _logger.error(f"Failed to delete user group quota policy. Status code: {response.status_code}, Response: {response.text}")
-        raise Exception(f"Failed to delete user group quota policy. Status code: {response.status_code}, Response: {response.text}")
 
 def setup(fa):
-    
     # Create local user
     try:
+        _logger.info("Creating local user demo.")
         fa.create_local_user(
-            name=os.getenv('FA_DEMO_USER_NAME', 'demo'), 
-            uid=1001, 
+            name=os.getenv('FA_DEMO_USER_NAME', 'demo'),
+            uid=1001,
             enabled=True,
-            primary_group=ReferenceWithType(name='Administrators'), 
+            primary_group=ReferenceWithType(name='Administrators'),
             password=os.getenv('FA_DEMO_USER_PASSWORD', 'password')
         )
     except Exception as e:
@@ -72,62 +34,93 @@ def setup(fa):
 
     # Create filesystem
     try:
-        fa.create_file_system('user_group_quota_file_system')
+        _logger.info("Creating file system 'user_quota_file_system'.")
+        fa.create_file_system('user_quota_file_system')
     except Exception as e:
-        _logger.error(f"Error creating file system 'user_group_quota_file_system'. Error message '{e}'.")
+        _logger.error(f"Error creating file system 'user_quota_file_system'. Error message '{e}'.")
 
+    # Create NFS policies
     try:
-        create_policy_user_group_quota(name='user_group_quota_policy')
+        _logger.info("Creating NFS policy 'nfs_user_quota_access_policy'.")
+        fa.create_policy_nfs(name='nfs_user_quota_access_policy', disable_user_mapping=False)
+        fa.create_policy_nfs_rule(policy_name='nfs_user_quota_access_policy', client='*', access='no-root-squash',
+                                  nfs_version='nfsv4', security='auth_sys',
+                                  permission='rw')
     except Exception as e:
-        _logger.error(f"Error creating policy 'user_group_quota_policy'. Error message '{e}'.")
+        _logger.error(f"Error creating policy 'nfs_user_quota_access_policy'. Error message '{e}'.")
 
+    # Create SMB policies
     try:
-        fa.export_managed_directory_smb(policy_name='smb-simple',
-                                        managed_directory_name='user_group_quota_file_system:root',
-                                        export_name='user_group_quota')
+        _logger.info("Creating SMB policy 'smb_user_quota_access_policy'.")
+        fa.create_policy_smb(name='smb_user_quota_access_policy')
+        fa.create_policy_smb_rule(policy_name='smb_user_quota_access_policy', client='*')
     except Exception as e:
-        _logger.error(f"Error exporting managed directory for 'user_group_quota_policy'. Error message '{e}'.")
+        _logger.error(f"Error creating policy 'smb_user_quota_access_policy'. Error message '{e}'.")
+
+    # Export managed directory
+    try:
+        _logger.info("Exporting managed directory over NFS.")
+        fa.export_managed_directory_nfs(policy_name='nfs_user_quota_access_policy',
+                                        managed_directory_name='user_quota_file_system:root',
+                                        export_name='user_quota_export')
+
+        _logger.info("Exporting managed directory over SMB.")
+        fa.export_managed_directory_smb(policy_name='smb_user_quota_access_policy',
+                                        managed_directory_name='user_quota_file_system:root',
+                                        export_name='user_quota_share')
+    except Exception as e:
+        _logger.error(f"Error exporting managed directory. Error message '{e}'.")
+
 
 def cleanup(fa):
 
     # Delete exports and policies
     try:
-        fa.delete_export(export_name='user_group_quota', policy_name='smb-simple')
+        _logger.info("Deleting NFS and SMB exports.")
+        fa.delete_export(export_name='user_quota_export', policy_name='nfs_user_quota_access_policy')
+        fa.delete_export(export_name='user_quota_share', policy_name='smb_user_quota_access_policy')
     except Exception as e:
-        _logger.error(f"Error deleting export 'user_group_quota'. Error message '{e}'.")
+        _logger.error(f"Error deleting NFS and SMB exports. Error message '{e}'.")
 
-    # Destroy and eradicate file system
+    # Delete file system
     try:
-        fa.destroy_file_system(name='user_group_quota_file_system')
-        fa.eradicate_file_system(name='user_group_quota_file_system')
+        _logger.info("Destroying file system 'user_quota_file_system'.")
+        fa.destroy_file_system(name='user_quota_file_system')
+        _logger.info("Eradicating file system 'user_quota_file_system'.")
+        fa.eradicate_file_system(name='user_quota_file_system')
     except Exception as e:
-        _logger.error(f"Error destroying or eradicating file system 'user_group_quota_file_system'. Error message '{e}'.")
+        _logger.error(f"Error deleting file system 'user_quota_file_system'. Error message '{e}'.")
 
     # Delete policies
     try:
-        fa.delete_policy_user_group_quota(name='user_group_quota_policy')
+        _logger.info("Deleting NFS and SMB policies.")
+        fa.delete_policy_nfs(name='nfs_user_quota_access_policy')
+        fa.delete_policy_smb(name='smb_user_quota_access_policy')
     except Exception as e:
-        _logger.error(f"Error deleting policy 'user_group_quota_policy'. Error message '{e}'.")
+        _logger.error(f"Error deleting policies. Error message '{e}'.")
 
     # Delete local user
     try:
+        _logger.info("Deleting local user 'demo'.")
         fa.delete_local_user(name=os.getenv('FA_DEMO_USER_NAME', 'demo'))
     except Exception as e:
         _logger.error(f"Error deleting local user. Error message '{e}'.")
 
+
 if __name__ == '__main__':
 
-    # Setup connection to FlashArray
+    # Setup SOCKS5 proxy
+
     FA_HOSTNAME = os.getenv("FA_DEMO_HOSTNAME")
     FA_API_TOKEN = os.getenv("FA_DEMO_API_TOKEN")
-    
+
     '''
     import socket
     import socks
 
     FA_DEMO_USE_SOCKS5_PROXY = os.getenv("FA_DEMO_USE_SOCKS5_PROXY")
 
-    
+
     if FA_DEMO_USE_SOCKS5_PROXY == 'true':
         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 1080)
         socket.socket = socks.socksocket
@@ -139,6 +132,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "cleanup":
         _logger.info(f"Cleaning up {FA_HOSTNAME} array ...")
         cleanup(fa)
-    else:    
+    else:
         _logger.info(f"Configuring {FA_HOSTNAME} array ...")
         setup(fa)
